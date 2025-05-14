ebc4e3ee153b164fb6da6bbf1eccf2a4fe995dcb
KRISTIN\racoo <racoonbean@gmail.com>
2025-05-14 21:04:20 +0900

    no message

---
+++ 20250514/js/common.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.main-swiper',{
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
